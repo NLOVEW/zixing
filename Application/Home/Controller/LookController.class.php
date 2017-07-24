3b87a9932d64fb8988e8708d94204fb4bee5488d
luck_nhb <1443793484@qq.com>
2017-07-24 15:10:34 +0800

    ppt,execl,word在线浏览功能

--- Application/Home/Controller/LookController.class.php
+++ Application/Home/Controller/LookController.class.php
@@ -1,18 +1,43 @@
 <?php
     namespace Home\Controller;
     use Think\Controller;
+    vendor('OfficeToPdf.OfficeToPdf', '' ,'.php');
     header("Content-type: text/html; charset=utf-8");
     class LookController extends Controller{
+
+
         function lookPdf(){
+            $this->assign("path",'/Public/Resource/Pdf/test.pdf');
             $this->display();
         }
+
         function lookWord(){
+            $pdf=new \OfficeToPdf();
+            $inputPath = 'D:/PhpStudy/WWW/zixing/Public/Resource/Word'.'/test.docx';
+            $outputPath = 'D:/PhpStudy/WWW/zixing/Public/Resource/Word/wordPdf'.'/test.pdf';
+            $pdf->run($inputPath,$outputPath);
+            $outputPath=str_replace("D:/PhpStudy/WWW/zixing/Public","/Public",$outputPath);
+            $this->assign('path',$outputPath);
             $this->display();
         }
-        function viewer(){
+
+        function lookPpt(){
+            $pdf=new \OfficeToPdf();
+            $inputPath = 'D:/PhpStudy/WWW/zixing/Public/Resource/PPT'.'/test.pptx';
+            $outputPath = 'D:/PhpStudy/WWW/zixing/Public/Resource/PPT/pptPdf'.'/test.pdf';
+            $pdf->run($inputPath,$outputPath);
+            $outputPath=str_replace("D:/PhpStudy/WWW/zixing/Public","/Public",$outputPath);
+            $this->assign('path',$outputPath);
             $this->display();
         }
-        function viewer2(){
+
+        function lookExecl(){
+            $pdf=new \OfficeToPdf();
+            $inputPath = 'D:/PhpStudy/WWW/zixing/Public/Resource/Execl'.'/test.xlsx';
+            $outputPath = 'D:/PhpStudy/WWW/zixing/Public/Resource/Execl/execlPdf'.'/test.pdf';
+            $pdf->run($inputPath,$outputPath);
+            $outputPath=str_replace("D:/PhpStudy/WWW/zixing/Public","/Public",$outputPath);
+            $this->assign('path',$outputPath);
             $this->display();
         }
     }
