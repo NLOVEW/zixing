84ba60aa894af2b5b7e48996c28c7e0075c59b6d
luck_nhb <1443793484@qq.com>
2017-08-11 19:39:37 +0800

    缺少验证

--- Application/Runtime/Cache/Home/50c9f5d2ef37c2cf131d5cf1381d3a4b.php
+++ Application/Runtime/Cache/Home/50c9f5d2ef37c2cf131d5cf1381d3a4b.php
@@ -1,95 +1,86 @@
 <?php if (!defined('THINK_PATH')) exit();?>﻿<!DOCTYPE HTML>
 <html>
 <head>
-<meta charset="utf-8">
-<meta name="renderer" content="webkit|ie-comp|ie-stand">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=2.0, user-scalable=yes" />
-<!--[if lt IE 9]>
-<script type="text/javascript" src="/Public/lib/html5shiv.js"></script>
-<script type="text/javascript" src="/Public/lib/respond.min.js"></script>
-<![endif]-->
-<link rel="stylesheet" type="text/css" href="/Public/static/h-ui/css/H-ui.min.css" />
-<link rel="stylesheet" type="text/css" href="/Public/static/h-ui.admin/css/H-ui.login.css" />
-<link rel="stylesheet" type="text/css" href="/Public/static/h-ui.admin/css/style.css" />
-<link rel="stylesheet" type="text/css" href="/Public/lib/Hui-iconfont/1.0.8/iconfont.css" />
-<!--[if IE 6]>
-<script type="text/javascript" src="/Public/lib/DD_belatedPNG_0.0.8a-min.js"></script>
-<script>DD_belatedPNG.fix('*');</script>
-<![endif]-->
-<title>欢迎来到自兴人工智能学院</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>自兴人工智能学院后台管理</title>
+  <link rel="stylesheet" type="text/css" href="/Public/LoginConfig/assets/css/style.css" />
+  
 </head>
-<body>
-<input type="hidden" id="TenantId" name="TenantId" value="" />
-<div class="header"></div>
-<div class="loginWraper">
-  <div id="loginform" class="loginBox">
-    <form class="form form-horizontal" method="post" id="submit_form">
-      <div class="row cl">
-        <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe60d;</i></label>
-        <div class="formControls col-xs-8">
-          <input id="username" name="userId" type="text" placeholder="账户" class="input-text size-L">
+<body onLoad="sendRequest()" >
+<div class="videozz"></div>
+<div class="box">
+  <div class="box-a">
+    <div class="m-2">
+      <div class="m-2-1">
+        <form action="/index.php/Home/Login/loginCheck" method="post">
+          <div class="m-2-2">
+            <input name="userId" type="text" placeholder="请输入账号"  style="width: 350px;height: 50px"/>
+          </div>
+          <div class="m-2-2">
+            <input name="password" type="password" placeholder="请输入密码" style="width: 350px;height: 50px"/>
+          </div>
+          <div class="m-2-2-1">
+            <input name="code" type="text" placeholder="请输入验证码" style="width: 150px;height: 50px"/>
+            <img  id="code" src="/index.php/Home/Login/verify" onclick="this.src='/index.php/Home/Login/verify?'+Math.random()" style="width: 180px;height: 50px" alt="点击换一个"/>
+          </div>
+          <div class="m-2-2">
+            <button type="submit" value="登录" style="width: 350px;height: 50px;cursor: pointer"/> 登录
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="m-5">
+      <div id="m-5-id-1">
+        <div id="m-5-2">
+          <div id="m-5-id-2">
+          </div>
+          <div id="m-5-id-3"></div>
         </div>
       </div>
-      <div class="row cl">
-        <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe60e;</i></label>
-        <div class="formControls col-xs-8">
-          <input id="password" name="password" type="password" placeholder="密码" class="input-text size-L">
+    </div>
+    <div class="m-10"></div>
+    <div class="m-xz7"></div>
+    <div class="m-xz8 xzleft"></div>
+    <div class="m-xz9"></div>
+    <div class="m-xz9-1"></div>
+    <div class="m-x17 xzleft"></div>
+    <div class="m-x18"></div>
+    <div class="m-x19 xzleft"></div>
+    <div class="m-x20"></div>
+    <div class="m-8"></div>
+    <div class="m-9"><div class="masked1" id="sx8">自兴人工智能</div></div>
+    <div class="m-11">
+      <div class="m-k-1"><div class="t1"></div></div>
+      <div class="m-k-2"><div class="t2"></div></div>
+      <div class="m-k-3"><div class="t3"></div></div>
+      <div class="m-k-4"><div class="t4"></div></div>
+      <div class="m-k-5"><div class="t5"></div></div>
+      <div class="m-k-6"><div class="t6"></div></div>
+      <div class="m-k-7"><div class="t7"></div></div>
+    </div>
+    <div class="m-14"><div class="ss"></div></div>
+    <div class="m-15-a">
+      <div class="m-15-k">
+        <div class="m-15xz1">
+          <div class="m-15-dd2"></div>
         </div>
       </div>
-      <div class="row cl">
-        <div class="formControls col-xs-8 col-xs-offset-3"  id="code">
-          <input name="code" id="verify" class="input-text size-L" type="text" placeholder="验证码" style="width:150px;">
-          <img src="/index.php/Home/Login/verify">
-        </div>
-      </div>
-      <div class="row cl">
-        <div class="formControls col-xs-8 col-xs-offset-3">
-          <label for="online">
-            <input type="checkbox" name="rember" id="online" value="" style="height: 15px;width: 15px">
-            记住密码</label>
-        </div>
-      </div>
-      <div class="row cl">
-        <div class="formControls col-xs-8 col-xs-offset-3">
-          <input name="" type="submit" class="btn btn-success radius size-L" value="&nbsp;登&nbsp;&nbsp;&nbsp;&nbsp;录&nbsp;" onclick="check()">
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <input name="" type="reset" class="btn btn-default radius size-L" value="&nbsp;取&nbsp;&nbsp;&nbsp;&nbsp;消&nbsp;">
-        </div>
-      </div>
-    </form>
+    </div>
+    <div class="m-16"></div>
+    <div class="m-17"></div>
+    <div class="m-18 xzleft"></div>
+    <div class="m-19"></div>
+    <div class="m-20 xzleft"></div>
+    <div class="m-21"></div>
+    <div class="m-22"></div>
+    <div class="m-23 xzleft"></div>
+    <div class="m-24" id="localtime"></div>
   </div>
 </div>
-<div class="footer">© 自兴人工智能</div>
-<script type="text/javascript" src="/Public/lib/jquery/1.9.1/jquery.min.js"></script>
-<script type="text/javascript" src="/Public/static/h-ui/js/H-ui.min.js"></script>
+<script type="text/javascript" src="/Public/LoginConfig/assets/js/common.min.js"></script>
+</body>
 <script>
-    var img = $('#code').find('img')
-    var verify_img = img.attr("src");
-    img.attr('title', '点击刷新');
-    img.click(function(){
-        if( verify_img.indexOf('?')>0){
-            $(this).attr("src", verify_img+'&random='+Math.random());
-        }else{
-            $(this).attr("src", verify_img.replace(/\?.*$/,'')+'?'+Math.random());
-        }
-    });
-</script>
-
-<script type="text/javascript">
-    function check() {
-        var username = $("#username").val();
-        var password = $("#password").val();
-        var verify = $("#verify").val();
-        if(username==''){
-            alert("请输入账号");
-        }else if(password==''){
-            alert("密码不能为空");
-        }else if(verify==''){
-            alert("验证码不能为空");
-        }else{
-            document.getElementById("submit_form").action="/index.php/Home/Login/loginCheck";
-        }
-    }
 
 </script>
+</html>
